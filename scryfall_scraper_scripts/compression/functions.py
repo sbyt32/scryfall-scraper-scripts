0ea7bdc4476270c376f6a65ca5982e828aed60ba
sbyt32 <sbyt32@gmail.com>
2023-02-22 19:40:37 -0800

    fix file error

--- scryfall_scraper_scripts/compression/functions.py
+++ scryfall_scraper_scripts/compression/functions.py
@@ -7,18 +7,20 @@ from typing import Optional
 
 def compress_dir(zip_path: Optional[str] = "", f_path: Optional[str] = "") -> None:
     """
+    Compress a whole directory! I hope it works!
+
     f_path: Location of the .json files to compress. Default current directory.
     zip_path: Location of the .gz file. Default current directory.
     """
     if not f_path:
         f_path = ""
-    files: list = glob.glob(f"{f_path}*.json")
 
+    files: list = [os.path.basename(files) for files in glob.glob(f"{f_path}*.json")]
     if not os.path.exists(zip_path):
         zip_path = "./"
 
     for file in files:
-        zip_path = f"{zip_path}{file}.gz"
-        if not os.path.exists(zip_path):
-            with gzip.open(zip_path, "wb") as compressed_fp:
-                shutil.copyfileobj(open(file, "rb"), compressed_fp)
+        file_path = f"{zip_path}{file}.gz"
+        if not os.path.exists(file_path):
+            with gzip.open(file_path, "wb") as compressed_fp:
+                shutil.copyfileobj(open(f"{f_path}{file}", "rb"), compressed_fp)
